b1a83d27a2f17c331978483604561535d0083517
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-05-22 23:13:44 +0100

    subreddits now stored in json and rendered from array

--- src/components/SubredditMenu.js
+++ src/components/SubredditMenu.js
@@ -1,22 +1,18 @@
 import React from "react";
 import '../stylesheets/SubredditMenu.css';
+import subredditList from '../data/subreddits.json';
+
 
 const SubredditMenu = () => {
     return (
         <div className="subredditMenu">
             <p>Subreddit</p>
             <ul>
-                <li><button>r/radiohead</button></li>
-                <li><button>r/britishproblems</button></li>
-                <li><button>r/PizzaCrimes</button></li>
-                <li><button>r/ProgrammerHumor</button></li>
-                <li><button>r/softwaregore</button></li>
-                <li><button>r/badcode</button></li>
-                <li><button>r/AmITheAsshole</button></li>
-                <li><button>r/chemicalreactiongifs</button></li>
-                <li><button>r/programming</button></li>
-                <li><button>r/UKhiking</button></li>
-                <li><button>r/shittyrobots</button></li>
+                {
+                    subredditList.map((subreddit, i) => (
+                        <li key={i}><button type="button">{subreddit}</button></li>
+                    ))
+                }
             </ul>
         </div>
     );
