b1661bbeb443e7bb297d028f165ffa1c61976990
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-05-22 22:53:11 +0100

    added hardcoded example subreddits

--- src/components/SubredditMenu.js
+++ src/components/SubredditMenu.js
@@ -5,6 +5,19 @@ const SubredditMenu = () => {
     return (
         <div className="subredditMenu">
             <p>Subreddit</p>
+            <ul>
+                <li><button>r/radiohead</button></li>
+                <li><button>r/britishproblems</button></li>
+                <li><button>r/PizzaCrimes</button></li>
+                <li><button>r/ProgrammerHumor</button></li>
+                <li><button>r/softwaregore</button></li>
+                <li><button>r/badcode</button></li>
+                <li><button>r/AmITheAsshole</button></li>
+                <li><button>r/chemicalreactiongifs</button></li>
+                <li><button>r/programming</button></li>
+                <li><button>r/UKhiking</button></li>
+                <li><button>r/shittyrobots</button></li>
+            </ul>
         </div>
     );
 };
