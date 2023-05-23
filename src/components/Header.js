d2062926cb9b3e719ae132c34725a6a37f735be6
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-05-23 23:59:48 +0100

    added inline theme styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useThemeColors, useThemeObject } from '../hooks/themeHooks';
 import '../stylesheets/Header.css'
 import DisplayToggle from './DisplayToggle';
 import Searchbar from './Searchbar';
 
 const Header = () => {
+
+    const background = useThemeObject("backgroundColor", "background");
+    const accentColor = useThemeObject("color", "accent");
+    const borderColor = useThemeObject("color", "secondaryText");
+
     return (
-        <div className="header">
-            <h1 className="logo"><span className="karma">KARMA</span><span className="police">POLICE</span></h1>
+        <div className="header" style={background}>
+            <h1 className="logo">
+                <span className="karma" style={accentColor}>KARMA</span>
+                <span className="police" style={borderColor}>POLICE</span>
+            </h1>
             <div className="rightSide">
-                <DisplayToggle id="icon"/>
+                <DisplayToggle id="icon" />
                 <Searchbar />
             </div>
         </div>
