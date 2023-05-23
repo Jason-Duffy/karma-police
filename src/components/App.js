2c8074110d434c420c5073ab9dc4aaa73dfadd06
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-05-23 22:28:21 +0100

    refactored for useThemeColors hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectTheme } from '../redux/themeSlice';
+import { useThemeColors } from '../hooks/themeHooks';
 import '../stylesheets/App.css';
 import '../stylesheets/globalStyles.css';
 import Header from './Header';
@@ -11,15 +12,20 @@ import SubredditMenu from './SubredditMenu';
 export default function App() {
 
   const theme = useSelector(selectTheme);
+  const themeColors = useThemeColors();
 
   // Update the CSS class of the body when the theme changes.
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
 
+  // Set body element to background colour from theme.
+  const background = themeColors.background;
+  document.body.style.backgroundColor = background;
+
 
   return (
-    <div className="page-container">
+    <div className="page-container" style={{backgroundColor: background}}>
       <div className="container-wrap">
         <div className="App">
           <Header />
