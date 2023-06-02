d251106e05eaf2a546f30521689cf5dfa4cdd954
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-02 17:54:59 +0100

    Moved PostText to separate component
    Added functionality to ensure relative links inside postText html are opened with target: blank and prepended with the reddit base URL.

--- src/components/Posts/Card/CardContent/PostText/PostText.js
+++ src/components/Posts/Card/CardContent/PostText/PostText.js
@@ -1,8 +1,13 @@
-import React from "react";
+// React module imports.
+import React, { useEffect, useRef } from "react";
 import styled from 'styled-components';
-
+// Local imports.
 import { useThemeColors } from "../../../../../hooks/themeHooks";
+// Style imports. 
 
+
+// Create styled component for post text. 
+// Workaround for styling child html entities whilst using dangerouslySetInnerHtml
 const PostTextContainer = styled.div`
     a {
         color: ${props => props.linkColor};
@@ -21,10 +26,39 @@ const PostTextContainer = styled.div`
     table {
         width: 40%;
     }
+
+    th {
+        color: ${props => props.headingColor};
+    }
+
+    tr {
+        color: ${props => props.textColor};
+    }
     `;
 
 const PostText = ({ decodedText }) => {
 
+    // Create a reference to the post text container. 
+    const postTextRef = useRef(null);
+    const host = window.location.origin;
+    const reddit = 'https://www.reddit.com/';
+
+    useEffect(() => {
+        if (postTextRef.current) {
+            const links = postTextRef.current.getElementsByTagName('a');
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].href.startsWith(host)) {
+                    links[i].href = links[i].href.replace(host, reddit);
+                }
+                links[i].target = '_blank';
+                links[i].rel = 'noopener noreferrer';
+            }
+        }
+    }, [decodedText]);
+    
+
+
+    // Get theme color variables. 
     const themeColors = useThemeColors();
 
     const linkColor = themeColors.accent;
@@ -38,6 +72,7 @@ const PostText = ({ decodedText }) => {
             linkColor={linkColor}
             textColor={textColor}
             headingColor={headingColor}
+            ref={postTextRef}
         />
     );
 };
