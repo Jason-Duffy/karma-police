acb461c70ba98798b5a301626acc586b4c7e46c7
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-05 12:26:42 +0100

    Wrapped App component in Provider to give access to Redux store

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './containers/App/App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 test('renders header text', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const headerText = screen.getByText(/KARMA POLICE/i);
   expect(headerText).toBeInTheDocument();
 });
