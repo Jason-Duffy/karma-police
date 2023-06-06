54169d77158f72e5f8274680d8780ce69d631960
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-07 00:02:40 +0100

    SubredditList now renders styled NavLink

--- src/elements/SubredditList/SubredditList.js
+++ src/elements/SubredditList/SubredditList.js
@@ -1,46 +1,43 @@
 // React module imports.
-import React from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 // Local imports.
 import subredditList from '../../assets/data/subreddits.json';
-import { setSubreddit, selectSubreddit } from "../../redux/subredditSlice";
 import { useThemeObject } from "../../hooks/themeHooks";
-// Style imports.
 import './SubredditList.css';
+
+// Create a styled component for your links
+const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+
+    color: ${props => props.secondarytext};
+    &.active {
+        font-weight: bold;
+        font-size: larger;
+        color: ${props => props.accentcolor};
+    }
+`;
 
 const SubredditList = () => {
 
     const secondaryText = useThemeObject("color", "secondaryText");
     const accentColor = useThemeObject("color", "accent");
 
-    // Get and manage the current subreddit
-    const currentSubreddit = useSelector(selectSubreddit);
-
-    const dispatch = useDispatch();
-    const changeSubreddit = (subreddit) => () => {
-        dispatch(setSubreddit(subreddit));
-    };
-
     return (
         <div>
             <ul id="sr-list" style={secondaryText}>
                 {
                     subredditList.map((subreddit, i) => {
-                        // Create a style object for the subreddit button
-                        const buttonStyle = currentSubreddit === subreddit
-                            ? { ...secondaryText, textDecoration: 'underline', fontWeight: 'bold', color: accentColor.color } // active style
-                            : secondaryText; // default style
-
                         return (
                             <li id="sr-name" key={i}>
-                                <button
-                                    id="sr-button"
-                                    type="button"
-                                    style={buttonStyle}
-                                    onClick={changeSubreddit(subreddit)}
+                                <StyledNavLink 
+                                    to={`/${subreddit}`}
+                                    secondarytext={secondaryText.color}
+                                    accentcolor={accentColor.color}
                                 >
                                     {subreddit}
-                                </button>
+                                </StyledNavLink>
                             </li>
                         );
                     })
@@ -48,6 +45,6 @@ const SubredditList = () => {
             </ul>
         </div>
     );
-};
+}
 
 export default SubredditList;
