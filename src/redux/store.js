9225afa0ff0ca29aaa17da0cc4cd56b2b7b3fd13
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-07 00:06:33 +0100

    removed subredditSlice from store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './themeSlice';
 import sortReducer from './sortSlice';
-import subredditReducer from './subredditSlice';
 import menuButtonReducer from './menuButtonSlice';
 import subredditButtonReducer from './subredditButtonSlice';
 import subredditDataReducer from './subredditDataSlice';
@@ -14,7 +13,6 @@ export default configureStore({
   reducer: {
     theme: themeReducer,
     sort: sortReducer,
-    subreddit: subredditReducer,
     menu: menuButtonReducer,
     subredditButton: subredditButtonReducer,
     subredditData: subredditDataReducer,
