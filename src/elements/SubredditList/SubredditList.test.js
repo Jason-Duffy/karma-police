a2f5fa7c0b443f72e82c845292da14a208aba0dd
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-12 12:56:53 +0100

    Revert "Added initial unit test"
    This reverts commit e1ad4dfe8413d3916688af20ae276c6f6da625d2.

--- src/elements/SubredditList/SubredditList.test.js
+++ src/elements/SubredditList/SubredditList.test.js
@@ -1,33 +1,10 @@
-// React moduke imports.
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import { render, screen, cleanup } from '@testing-library/react';
-// Local imports.
 import SubredditList from './SubredditList';
 import store from '../../redux/store';
 
-/*
-test('renders subreddit list correctly', () => {
-    const subredditList = ['reactjs', 'javascript', 'webdev'];
-  
-    render(
-      <Provider store={store}>
-        <Router>
-          <SubredditList />
-        </Router>
-      </Provider>
-    );
-    
-    const listItems = document.querySelectorAll('#sr-list > li');
-  
-    expect(listItems.length).toBe(subredditList.length);
-    
-    listItems.forEach((listItem, index) => {
-      expect(listItem.textContent).toBe(subredditList[index]);
-    });
-  });
-*/
 
 test('should render subredditList component', () => {
     render(
