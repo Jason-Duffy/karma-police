8aa1f7a0a9eabe1e6ab5ade3431dbfe7d762c953
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-13 20:26:29 +0100

    added description to error code

--- src/components/Posts/ErrorCard/ErrorCard.js
+++ src/components/Posts/ErrorCard/ErrorCard.js
@@ -1,20 +1,23 @@
 // React module imports.
 import React from "react";
+import { getReasonPhrase } from 'http-status-codes';
 // Local imports.
 import { useThemeObject } from "../../../hooks/themeHooks";
 // Style imports.
 import "../NoSearchResultsCard/NoSearchResultsCard.css"
 
-const ErrorCard = (errorCode) => {
+const ErrorCard = ({errorCode}) => {
 
     // Inline style variables.
     const primaryText = useThemeObject("color", "primaryText");
     const borderColor = useThemeObject("backgroundColor", "border");
 
+    const statusDescription = getReasonPhrase(errorCode);
+
     return (
         <div className="no-search-results-card-container" style={borderColor}>
             <div className="no-search-results-card">
-                <p style={primaryText}>Whoops! There's been an error. Try another subreddit.</p>
+                <p style={primaryText}>Whoops! There's been an error: {errorCode} {statusDescription}. Try another subreddit.</p>
             </div>
         </div>
     );
