1e807b8f2adc8b577d352f55efa46393589d2ee7
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-14 20:42:02 +0100

    Added react tests

--- src/elements/SubredditList/SubredditList.test.js
+++ src/elements/SubredditList/SubredditList.test.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
-import { render, screen, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import SubredditList from './SubredditList';
+import subredditList from '../../assets/data/subreddits.json';
 import store from '../../redux/store';
 
 
-test('should render subredditList component', () => {
-    render(
-        <Provider store={store}>
-            <Router>
-                <SubredditList />
-            </Router>
-        </Provider>
-    );
-    const subredditListElement = screen.getByTestId('subredditList-1');
-    expect(subredditListElement).toBeInTheDocument();
-})
+describe('SubredditList component', () => {
+    test('should render subredditList component', () => {
+        render(
+            <Provider store={store}>
+                <Router>
+                    <SubredditList />
+                </Router>
+            </Provider>
+        );
+        const subredditListElement = screen.getByTestId('subredditList-1');
+        expect(subredditListElement).toBeInTheDocument();
+    })
+
+    test('should render the correct number of list items', () => {
+        render(
+            <Provider store={store}>
+                <Router>
+                    <SubredditList />
+                </Router>
+            </Provider>
+        );
+        const listItems = screen.getAllByRole('listitem');
+        expect(listItems).toHaveLength(subredditList.length);
+    });
+
+    test('should render active link on click', () => {
+        const subreddit = 'r/radiohead';
+        render(
+            <Provider store={store}>
+                <Router>
+                    <SubredditList />
+                </Router>
+            </Provider>
+        );
+        const subredditLink = screen.getByText(subreddit);
+        fireEvent.click(subredditLink);
+        expect(subredditLink).toHaveClass('active');
+    });
+});
