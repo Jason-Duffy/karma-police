440c69bb9090d77e1a745034e346838af8a4ad32
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-17 23:23:04 +0100

    added workaround to suppress console error

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    setupFiles: ['<rootDir>/src/setupTests.js'],
     collectCoverage: true,
     collectCoverageFrom: [
         '**/*.{js,jsx}',
         '!**/node_modules/**',
         '!**/vendor/**',
-      ],
-  };
-  
-  module.exports = config;
+    ],
+};
+
+module.exports = config;
