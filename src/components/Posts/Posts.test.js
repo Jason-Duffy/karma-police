bdd4500690a0cbb2b0c9387428c9040fe9f0c815
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-18 00:47:55 +0100

    tidied up

--- src/components/Posts/Posts.test.js
+++ src/components/Posts/Posts.test.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { render, screen, cleanup, fireEvent, getByRole } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import Posts from './Posts';
 import store from '../../redux/store';
 
 describe('Posts component', () => {
+
     test('should render Posts component', () => {
         const { container } = render(
             <Provider store={store}>
                 <Posts />
             </Provider>
-        )
+        );
         expect(container.firstChild).toBeInTheDocument();
     });
 
     test('', () => {
-
+        // Additional tests...
     });
 
     test('', () => {
-
+        // Additional tests...
     });
 
     test('', () => {
-
+        // Additional tests...
     });
 });
