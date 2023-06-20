b1778f2f3ffe19ca6e7c7ee2af509464bc6401bf
Jason Duffy <55949529+Jason-Duffy@users.noreply.github.com>
2023-06-20 22:52:04 +0100

    completed end-to-end test suite for arrest action

--- test/arrestAction.js
+++ test/arrestAction.js
@@ -1,16 +1,33 @@
 // Test environment dependencies
 const { By, Key, Builder, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 var should = require("chai").should();
 require("chromedriver");
-
 
 describe('Arrest action', () => {
     let driver;
     let usernameFromPost;
 
-    // Launch the browser before all test cases in the describe block
+    // Helper function to reliably find and click elements.
+    const findAndClickElement = async ( xpath ) => {
+        await driver.wait(until.elementLocated(By.xpath(xpath)));
+        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath(xpath))));
+        await driver.findElement(By.xpath(xpath)).click();
+    }
+    
     before(async () => {
-        driver = await new Builder().forBrowser("chrome").build();
+        // Configure Chrome options
+        const chromeOptions = new chrome.Options();
+
+        // Create the driver with desired capabilities
+        chromeOptions.addArguments("--ignore-certificate-errors");
+        chromeOptions.addArguments("--ignore-ssl-errors");
+        driver = await new Builder()
+            .forBrowser("chrome")
+            .setChromeOptions(chromeOptions)
+            .build();
+
+        // Launch the browser before all test cases in the describe block.
         await driver.get("https://jason-duffy.github.io/karma-police");
         await driver.wait(until.elementLocated(By.className("arrest")));
     });
@@ -86,18 +103,20 @@ describe('Arrest action', () => {
     });
 
     it('clicking "release all" should release all users & re-render their posts', async () => {
-        // Add first 3 users to arrested list. 
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="card-no-0"]/div[3]')));
-        await driver.findElement(By.xpath('//*[@id="card-no-0"]/div[3]')).click();
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="card-no-1"]/div[3]')));
-        await driver.findElement(By.xpath('//*[@id="card-no-1"]/div[3]')).click();
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="card-no-2"]/div[3]')));
-        await driver.findElement(By.xpath('//*[@id="card-no-2"]/div[3]')).click();
+        // xpath variable for first card in the page.
+        const cardPath = '//*[@id="card-no-0"]/div[3]';
 
-        // Get list of arrested users. 
+        // Click "Arrest" on the first 3 posts.
+        for (let i = 0; i < 3; ++i) {
+            await findAndClickElement(cardPath);
+        }
+        // Wait for the last card to disappear so the release button is clickable.
+        await driver.sleep(500);
+
+        // Get list of arrested users as elements. 
         let arrestsList = await driver.findElements(By.id('arrested-name'));
 
-        // Convert to array of strings.
+        // Convert element list to array of strings.
         let arrestsListText = [];
         for (let element of arrestsList) {
             let username = await element.getText();
@@ -105,12 +124,11 @@ describe('Arrest action', () => {
                 arrestsListText.push(username);
             }
         }
+        // xpath variable for Release button (full path). 
+        const buttonPath = '/html/body/div/div/div/div/div[4]/div/div[3]/div/button';
 
         // Get "release all" button and click it.
-        await driver.wait(until.elementLocated(By.id('release-all')));
-        let releaseAllButton = await driver.findElement(By.id('release-all'));
-        await driver.sleep(5000);
-        await releaseAllButton.click();
+        await findAndClickElement(buttonPath);
 
         // Get usernames from visible posts.
         let visiblePostUsernames = await driver.findElements(By.className('username'));
@@ -122,6 +140,7 @@ describe('Arrest action', () => {
             visiblePostUsernamesText.push(username);
         }
 
+        // -------- Assertions -------- //
         visiblePostUsernamesText.should.contain(arrestsListText[0]);
         visiblePostUsernamesText.should.contain(arrestsListText[1]);
         visiblePostUsernamesText.should.contain(arrestsListText[2]);
